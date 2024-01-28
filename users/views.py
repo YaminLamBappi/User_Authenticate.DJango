a8641eb38d1c1a07f2d9440dfab16ec2b35e0754
Yamin Lam Bappi <yaminlambappi@gmail.com>
2024-01-28 15:38:15 +0600

    Initial commit

---
+++ users/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForms
+
+def login_view(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username,'\n', password)
+        user = authenticate(request,username=username,password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('/signup/')
+        #    context = {"error": "Something Wrong!"}
+        else:
+            login(request, user)
+            return redirect('/admin')
+    return render(request, "accounts/login.html", context)
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('/login/')
+    return render(request, "accounts/logout.html", {})
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForms(request.POST)
+        if form.is_valid():
+             form.save()
+             return redirect('/login/')
+            
+    else: 
+        form = SignUpForms()
+    return render(request, 'accounts/signup.html', {'form' : form})
