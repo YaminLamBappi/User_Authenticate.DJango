52695c3b7f58193ace9ff3704965291bc538bc8b
Yamin Lam Bappi <57065050+YaminLamBappi@users.noreply.github.com>
2024-01-28 15:38:46 +0600

    Add files via upload

---
+++ user_authentication/users/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+class SignUpForms(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+            UserProfile.objects.create(user=user)
+        return user
